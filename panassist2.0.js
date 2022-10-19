a3d1a6550a8f761ff441a45d9edc281c685c90a1
c0rrupt <25bholland@shcp.edu>
2022-10-18 18:42:35 -0700

    Finished converting py code to JS

--- panassist2.0.js
+++ panassist2.0.js
@@ -52,3 +52,105 @@ function orange(rounds, wins){
 function gold(rounds, wins){
     return ((math.factorial((rounds + 1))/(math.factorial((wins + 1)) * math.factorial((rounds + 1) - (wins + 1)))) * ((0.028**(wins + 1)) * (0.972 ** ((rounds + 1) - (wins + 1)))))
 }
+
+function raw_probability(red_wins, black_wins, purple_wins, green_wins, yellow_wins, white_wins, orange_wins, gold_wins){
+    //  Calculates raw probablity next round
+
+    let rounds = red_wins + black_wins + purple_wins + green_wins + yellow_wins + white_wins + orange_wins + gold_wins
+
+    let raw_black = black(rounds, black_wins)
+    let raw_red = red(rounds, red_wins)
+    let raw_purple = purple(rounds, purple_wins)
+    let raw_green = green(rounds, green_wins)
+    let raw_yellow = yellow(rounds, yellow_wins)
+    let raw_white = white(rounds, white_wins)
+    let raw_orange = orange(rounds, orange_wins)
+    let raw_gold = gold(rounds, gold_wins)
+
+    let raw = [raw_black, raw_red, raw_purple, raw_green, raw_yellow, raw_white, raw_orange, raw_gold]
+    raw.sort(reverse = true)
+    console.log(raw)
+}
+
+function weighted_probability(red_wins, back_wins, purple_wins, green_wins, yellow_wins, white_wins, orange_wins, gold_wins){
+    //  Calculates weighted probability next round
+
+    let rounds = red_wins + black_wins + purple_wins + green_wins + yellow_wins + white_wins + orange_wins + gold_wins
+
+    let weighted_black = black(rounds, black_wins) * black_pay
+    let weighted_red = red(rounds, red_wins) * red_pay
+    let weighted_purple = purple(rounds, purple_wins) * purple_pay
+    let weighted_green = green(rounds, green_wins) * green_pay
+    let weighted_yellow = yellow(rounds, yellow_wins) * yellow_pay
+    let weighted_white = white(rounds, white_wins) * white_pay
+    let weighted_orange = orange(rounds, orange_wins) * orange_pay
+    let weighted_gold = gold(rounds, gold_wins) * gold_pay
+
+    let weighted = [weighted_black, weighted_red, weighted_purple, weighted_green, weighted_yellow, weighted_white, weighted_orange, weighted_gold]
+    weighted.sort(reverse = true)
+    console.log(weighted)
+}
+
+function coin_spread(red_wins, black_wins, purple_wins, green_wins, yellow_wins, white_wins, orange_wins, gold_wins){
+    //   Calculates optimal quarters to put on certain color
+
+    let rounds = red_wins + black_wins + purple_wins + green_wins + yellow_wins + white_wins + orange_wins + gold_wins
+
+    spread_red = raw_red/0.236
+    if (spread_red < 1){
+        let opt_spread_red = "0 quarters"}
+    if (spread_red == 1){
+        let opt_spread_red = "0-2 quarters"}
+    else{
+        let opt_spread_red = "3-4 quarters"}
+
+    spread_purple = raw_purple/0.088
+    if (spread_purple < 1){
+        let opt_spread_purple = "0 quarters"}
+    if (spread_purple == 1){
+        let opt_spread_purple = "0-2 quarters"}
+    else{
+        let opt_spread_purple = "3-4 quarters"}
+    
+    spread_green = raw_green/0.125
+    if (spread_green < 1){
+        let opt_spread_green = "0 quarters"}
+    if (spread_green == 1){
+        let opt_spread_green = "0-2 quarters"}
+    else{
+        let opt_spread_green = "3-4 quarters"}
+
+    spread_yellow = raw_yellow/0.111
+    if (spread_yellow < 1){
+        let opt_spread_yellow = "0 quarters"}
+    if (spread_yellow == 1){
+        let opt_spread_yellow = "0-2 quarters"}
+    else{
+        let opt_spread_yellow = "3-4 quarters"}
+
+    spread_white = raw_white/0.079
+    if (spread_white < 1){
+        let opt_spread_white = "0 quarters"}
+    if (spread_white == 1){
+        let opt_spread_white = "0-2 quarters"}
+    else{
+        let opt_spread_white = "3-4 quarters"}
+    spread_orange = raw_orange/0.046
+    if (spread_orange < 1){
+        let opt_spread_orange = "0 quarters"}
+    if (spread_orange == 1){
+        let opt_spread_orange = "0-2 quarters"}
+    else{
+        let opt_spread_orange = "3-4 quarters"}
+
+    spread_gold = raw_gold/0.028
+    if (spread_gold < 1){
+        let opt_spread_gold = "0 quarters"}
+    if (spread_gold == 1){
+        let opt_spread_gold = "0-2 quarters"}
+    else{
+        let opt_spread_gold = "3-4 quarters"}
+}
+
+let opt_spread = [opt_spread_red, opt_spread_purple, opt_spread_green, opt_spread_yellow, opt_spread_white, opt_spread_orange, opt_spread_gold]
+console.log(opt_spread)
